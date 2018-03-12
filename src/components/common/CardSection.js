7e8b4a31080429d8f588a039417e98d2123113f6
Juman Pak <qkrwnaks92@gmail.com>
2018-03-12 15:22:55 +0900

    Text item only click error fixed
    component......

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -15,8 +15,8 @@ const styles = {
     borderBottomWidth: 1,
     padding: 5,
     backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
+    // justifyContent: 'flex-start',
+    // flexDirection: 'row',
     borderColor: '#ddd',
     position: 'relative'
   }
